151b496845e17b53ff2e448e36c422092956355d
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-07 16:07:01 +0900

    실습 2 변형 2

--- main.c
+++ main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+//복사본 
 
-void func2(void) 
+void func(int x) 
  {
-     int x;
-     printf("func2 x is at %p\n", &x); 
-}
-
-void func(void) 
- {
-     int x;
      printf("func x is at %p\n", &x); 
-     func2();
+     x = 10;
 }
 
 int main(int argc, char *argv[]) 
@@ -19,7 +13,7 @@ int main(int argc, char *argv[])
     int x;
     
     printf("main x is at %p\n", &x);
-    func();
+    func(x); //변수 넣어줌 : 서로 다른 주소가 찍힘.(아예 다른 존재) 
   
   system("PAUSE");	
   return 0;
