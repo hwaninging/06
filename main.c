5da846e7e9e0faea2b7db241c7f8a1642572fd28
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-07 16:24:17 +0900

    실습3 디버거

--- main.c
+++ main.c
@@ -2,30 +2,29 @@
 #include <stdlib.h>
 //실습 3: 여러가지 함수 구현 
 
-//3가지 함수 정의
 int sumTwo(int a, int b)
 {
     return(a+b);
-} //몸체 
+} 
 
-int square(int n) //매개변수 한개
+int square(int n) 
 {
-    return (n*n); //제곱은 없어서 이렇게
+    return (n*n); 
 }
 
 int get_max(int a, int b)
 {
     if(a>b)
       return a;
-    else
+    else //체크 표시 뜨면 여기에 break가 걸린 것. (break point)
         return b; 
 }
 
 int main(int argc, char *argv[]) {
   
-  printf("sum two result is %i\n", sumTwo(2,3));
-  printf("square result is %i\n", square(5));
-  printf("getmax result is %i\n", get_max(10,13));
+  printf("sum two result is %i\n", sumTwo(2,3)); 
+  printf("square result is %i\n", square(5)); 
+  printf("getmax result is %i\n", get_max(10,13)); 
   
   system("PAUSE");	
   return 0;
