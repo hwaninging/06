378783ed6a2daf148b6e50976fef7bf11490c0e0
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-07 16:31:38 +0900

    실습 4
    Call By Value

--- main.c
+++ main.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-//실습 3: 여러가지 함수 구현 
+//실습 4: Call By Value - 메모리가 전달X, 값만 전달된다. 
 
-int sumTwo(int a, int b)
+int square(int a) //반환 해주기(제곱한걸 뽑고싶을때) void -> int 
 {
-    return(a+b);
-} 
-
-int square(int n) 
-{
-    return (n*n); 
+     return(a*a); //반환 a -> return 
 }
 
-int get_max(int a, int b)
-{
-    if(a>b)
-      return a;
-    else //체크 표시 뜨면 여기에 break가 걸린 것. (break point)
-        return b; 
-}
-
+// main함수 꾸미기 
 int main(int argc, char *argv[]) {
-  
-  printf("sum two result is %i\n", sumTwo(2,3)); 
-  printf("square result is %i\n", square(5)); 
-  printf("getmax result is %i\n", get_max(10,13)); 
+    
+    int a = 2;
+    a = square(a); //값이 2가 복사되어 들어감. (반환: square(a) -> a = square(a) 
+    printf("a=%i\n", a);
+
   
   system("PAUSE");	
   return 0;
